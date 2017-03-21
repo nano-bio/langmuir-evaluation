a75f79335dfae5ac63868a0e2663ca559fd660d9
Stefan Raggl <stefan.raggl@uibk.ac.at>
2017-03-21 15:55:52 +0100

    added effective electron density calculation and plot

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ parser.add_argument("--output",
                     "-o",
                     help="Specify a filename for the output data. Defaults to results.txt",
                     default='results.txt')
-					
+                    
 parser.add_argument("--probe_diameter",
                     "-pd",
-					type=float,
+                    type=float,
                     help="Specify the diameter of the probe. Defaults is set to 0.0004 m",
-                    default=0.0004)					
+                    default=0.0004)                    
 
 # parse it
 args = parser.parse_args()
@@ -45,6 +45,7 @@ probe_diam = args.probe_diameter # probe diameter in m
 # area of the langmuir probe
 probe_length = 0.003 # in meter 
 probe_area = probe_diam * pi * probe_length + probe_diam**2*pi/4  # in m^2
+print ('probe diameter = ', probe_diam)
 print ('probe area = ', probe_area)
 
 """
@@ -136,6 +137,8 @@ data_is_subtracted = np.zeros((lines - 1, nom), dtype=np.float64)
 temperatures = np.zeros((nom, 4), dtype=np.float64)
 # preallocate array for the ion density
 ion_density = np.zeros((nom, 1), dtype=np.float64)
+# preallocate array for effective electron density
+electron_density = np.zeros((nom, 1), dtype=np.float64)
 
 # go through all measurements
 for i in np.arange(0, nom):
@@ -236,7 +239,7 @@ for i in np.arange(0, nom):
         # if we happen to have only one electron temperature in the measurement, the fit converges to almost the same
         # electron temperature for t_hot and t_cold. hence we check, whether they are within a certain percentage of one
         # another and if so, we add the currents (as they are the same and save it as [t|n]_cold
-		# factor 1000000000 because of mA current signal and m^-3 to cm^-3 
+        # factor 1000000000 because of mA current signal and m^-3 to cm^-3 
         t_cold = p1[1]
         t_hot = p1[3]
         n_cold = p1[0] / (elementary_charge * probe_area) * np.sqrt(2 * pi * electron_mass / (Boltzmann * t_cold)) / 1000000000
@@ -246,7 +249,10 @@ for i in np.arange(0, nom):
         if t_cold > t_hot:
             t_cold, t_hot = t_hot, t_cold
             n_cold, n_hot = n_hot, n_cold
-
+            
+        # calculate total electron density 
+        electron_density[i] = n_cold + n_hot
+        
         if (t_hot / t_cold < (1 + hot_cold_diff)) & (t_hot / t_cold > (1 - hot_cold_diff)):
             t_hot = None
             n_cold += n_hot
@@ -255,7 +261,7 @@ for i in np.arange(0, nom):
         temperatures[i, :] = [t_hot, t_cold, n_hot, n_cold]
 
         # calculate ion density according to http://dx.doi.org/10.1116/1.1515800
-		# factor 1000000000 because of mA current signal and m^-3 to cm^-3 
+        # factor 1000000000 because of mA current signal and m^-3 to cm^-3 
         mass_argon = 39.96238 * physical_constants['atomic mass constant'][0]
         ion_density[i] = np.abs(ionsat[i](vp[i])) / (0.6 * elementary_charge ** (3 / 2) * probe_area) * np.sqrt(
             elementary_charge * mass_argon / (Boltzmann * t_cold)) / 1000000000
@@ -310,12 +316,12 @@ for i in np.arange(0, nom):
             fig1.suptitle('Filename {}'.format(filename))
 
 # export all data to a file
-export_values = np.concatenate((np.arange(1, nom+1).reshape((nom, 1)), vp, temperatures, ion_density, vf), axis=1)
+export_values = np.concatenate((np.arange(1, nom+1).reshape((nom, 1)), vp, temperatures, ion_density, vf, electron_density), axis=1)
 np.savetxt(output,
            export_values,
-           fmt=('%d', '%10.6f', '%10.2f', '%10.2f', '%1.4e', '%1.4e', '%1.4e', '%10.6f'),
+           fmt=('%d', '%10.6f', '%10.2f', '%10.2f', '%1.4e', '%1.4e', '%1.4e', '%10.6f', '%1.4e'),
            delimiter='\t',
-           header='Measurement\tPlasma Potential\tT_hot\tT_cold\tn_hot\tn_cold\tn_ion\tFloating Potential')
+           header='Measurement\tPlasma Potential\tT_hot\tT_cold\tn_hot\tn_cold\tn_ion\tFloating Potential\tEffective Electron Density')
 
 # make a nice overview plot using the export_values array
 fig2 = plt.figure()
@@ -353,6 +359,12 @@ ax5.plot(export_values[:, 0], export_values[:, 7], 'x')
 ax5.set_title('Floating Potential')
 ax5.set_xlabel('Angle')
 ax5.set_ylabel(r'$V_f$ (V)')
+
+ax6 = fig2.add_subplot(236)
+ax6.plot(export_values[:, 0], export_values[:, 8], 'x')
+ax6.set_title('Effective Electron Density')
+ax6.set_xlabel('Angle')
+ax6.set_ylabel(r'$n_{e,eff} (cm^{-3})$')
 
 fig2.tight_layout()
 fig2.suptitle('Filename {}'.format(filename))
